272891e3f3730d54643c96373cefe5b6697fba48
user <seomyungwon@gmail.com>
2026-04-28 15:53:39 +0900

    edit

--- lecture_3/Mordred.py
+++ lecture_3/Mordred.py
@@ -10,15 +10,15 @@ import os
 from mordred import Calculator, descriptors
 
 
-# 파일경로 설정
-current_directory = os.path.dirname(__file__)
-input_file = os.path.join(current_directory, "..", "dataset", "NPASS_NPs.xlsx")
+# 파일경로 설정 (자동설정)
+#current_directory = os.path.dirname(__file__)
+#input_file = os.path.join(current_directory, "..", "dataset", "NPASS_NPs.xlsx")
 
 
 # 파일경로 직접 설정
 #input_file = "userpath\\NPASS_NPs.xlsx"
-input_file = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPs.xlsx"
-output_file = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\Mordred_output.xlsx"
+input_file = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPs.xlsx"
+output_file = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_3_output\\Mordred_output.xlsx"
 
 #파일을 dataframe 형태로 불러오기
 df = pd.read_excel(input_file)
