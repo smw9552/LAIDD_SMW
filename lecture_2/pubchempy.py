d0d9a0ee5bc3d7ebaa4cf8a84ef1a70443adf82e
user <seomyungwon@gmail.com>
2026-04-28 15:48:01 +0900

    edit

--- lecture_2/pubchempy.py
+++ lecture_2/pubchempy.py
@@ -3,17 +3,28 @@
 # Date: 2023-10-20
 # E-mail: mwseo@krict.re.kr; seomyungwon@gmail.com
 
-import os
-import pandas as pd
+import ssl
+import urllib.request
 import pubchempy as pcp
+import pandas as pd
 
-# 파일경로 설정
-current_directory = os.path.dirname(__file__)
-input_file = os.path.join(current_directory, "..", "dataset", "NPASS_NPs.xlsx")
+context = ssl._create_unverified_context()
+
+# PubChemPy 내부 urlopen을 SSL 검증 해제 버전으로 강제 교체
+pcp.urlopen = lambda url, data=None, context=None: urllib.request.urlopen(
+    url,
+    data=data,
+    context=ssl._create_unverified_context()
+)
+
+
+# 파일경로 설정 (자동입력)
+#current_directory = os.path.dirname(__file__)
+#input_file = os.path.join(current_directory, "..", "dataset", "NPASS_NPs.xlsx")
 
 # 파일경로 직접 설정
 #input_file = "userpath\\NPASS_NPs.xlsx"
-input_file = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPs.xlsx"
+input_file = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPs.xlsx"
 
 
 #파일을 dataframe 형태로 불러오기
@@ -27,12 +38,16 @@ cid = df['PubChem_CID']
 cid_single = cid[0]
 
 #분자 불러오기
-c = pcp.Compound.from_cid(str(cid_single)) #CID 값을 문자열로 입력해야 함
-c = pcp.get_compounds('Eupatilin', 'name') #이름으로 검색
-c = pcp.get_compounds('COC1=C(C=C(C=C1)C2=CC(=O)C3=C(O2)C=C(C(=C3O)OC)O)OC', 'smiles') #SMIELS로 검색
-c = pcp.get_compounds('Eupatilin', 'name', record_type='3d') # 3D 구조 불러옴
+c_list = pcp.Compound.from_cid(str(cid_single), context=context) #CID 값을 문자열로 입력해야 함
+c_list = pcp.get_compounds('Eupatilin', 'name') #이름으로 검색
+c_list = pcp.get_compounds('COC1=C(C=C(C=C1)C2=CC(=O)C3=C(O2)C=C(C(=C3O)OC)O)OC', 'smiles') #SMIELS로 검색
+c_list = pcp.get_compounds('Eupatilin', 'name', record_type='3d') # 3D 구조 불러옴
 
 print(df)
+
+#분자 데이터 추출
+c = c_list[0]
+
 
 #분자 특성 불러오기
 c_mf = c.molecular_formula # molecular formula
