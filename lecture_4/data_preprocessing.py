5773d6dc9e539b7e465909150866965b8a0bee5c
user <seomyungwon@gmail.com>
2026-04-28 16:18:32 +0900

    edit

--- lecture_4/data_preprocessing.py
+++ lecture_4/data_preprocessing.py
@@ -21,12 +21,12 @@ from imblearn.over_sampling import SMOTE
 # output_file_final = os.path.join(current_directory, "..", "dataset", "output", "NPASS_NPT204_RDKit_activity_output_final.xlsx")
 
 # 파일경로 직접 설정
-input_file = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPT204_RDKit_activity_output.xlsx"
-output_file_nor = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\NPASS_NPT204_RDKit_activity_output_nor.xlsx"
-output_file_std = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\NPASS_NPT204_RDKit_activity_output_std.xlsx"
-output_file_ds = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\NPASS_NPT204_RDKit_activity_output_ds.xlsx"
-output_file_us = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\NPASS_NPT204_RDKit_activity_output_us.xlsx"
-output_file_final = "C:\\Users\\mwseo\\PycharmProjects\\LAIDD_SMW\\dataset\\output\\NPASS_NPT204_RDKit_activity_output_final.xlsx"
+input_file = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\NPASS_NPT204_RDKit_activity_output.xlsx"
+output_file_nor = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_4_output\\NPASS_NPT204_RDKit_activity_output_nor.xlsx"
+output_file_std = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_4_output\\NPASS_NPT204_RDKit_activity_output_std.xlsx"
+output_file_ds = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_4_output\\NPASS_NPT204_RDKit_activity_output_ds.xlsx"
+output_file_us = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_4_output\\NPASS_NPT204_RDKit_activity_output_us.xlsx"
+output_file_final = "C:\\Users\\user\\PycharmProjects\\LAIDD_SMW\\dataset\\lecture_4_output\\NPASS_NPT204_RDKit_activity_output_final.xlsx"
 
 #파일을 dataframe 형태로 불러오기
 df = pd.read_excel(input_file)
@@ -60,22 +60,26 @@ data = np.array(df_data)
 target = np.array(label)
 
 #total
+print("Total dataset")
 print(str("total data:"), len(target))
 #positive
 print(str("positive data:"), sum(target))
 #negative
 print(str("negative data:"), len(target) - sum(target))
+print("######################")
 
 # positive 데이터 수를 기준으로 negative 개수를 지정 (1:1 비율로 수정)
 sampler = RandomUnderSampler(sampling_strategy={0: len(np.where(target == 1)[0]), 1: len(np.where(target == 1)[0])})
 data_downsampled, target_downsampled = sampler.fit_resample(data, target)
 
 #total
+print("undersampling-RandomUnderSampler")
 print(str("total data:"), len(target_downsampled))
 #positive
 print(str("positive data:"), sum(target_downsampled))
 #negative
 print(str("negative data:"), len(target_downsampled) - sum(target_downsampled))
+print("######################")
 
 #파일작성
 df_data_down = pd.DataFrame(data_downsampled) #dataframe 구성
@@ -86,31 +90,34 @@ df_data_down.to_excel(output_file_ds, index=False) #데이터 확인
 
 
 #total
+print("Total dataset")
 print(str("total data:"), len(target))
 #positive
 print(str("positive data:"), sum(target))
 #negative
 print(str("negative data:"), len(target) - sum(target))
+print("######################")
+
 
 #Over-sampling
 seed = 2023
-smote = SMOTE(random_state=seed, sampling_strategy='auto') #sampling_stragety는 smapling 비율 설정
+smote = SMOTE(random_state=seed, sampling_strategy='auto') #sampling_stragety는 sampling 비율 설정
 data_smote, target_smote = smote.fit_resample(data, target)
 
 #total
+print("oversampling-SMOTE")
 print(str("total data:"), len(target_smote))
 #positive
 print(str("positive data:"), sum(target_smote))
 #negative
 print(str("negative data:"), len(target_smote) - sum(target_smote))
+print("######################")
 
 df_data_up = pd.DataFrame(data_smote) #dataframe 구성
 data_columns = df.columns[5:len(df.columns)] #df의 column 정보 가져오기 (0~4는 descriptor 정보 아님)
 df_data_up.columns = data_columns #column명 변경
 df_data_up.insert(0, 'label', target_smote) #label column 추가
 df_data_up.to_excel(output_file_us, index=False) #데이터 확인
-
-
 
 
 ## [실습3] 데이터 전처리 + 데이터 샘플링 ##
@@ -129,7 +136,7 @@ data = np.array(df_data_nor) #정규화 진행한 데이터 입력
 target = np.array(label)
 
 seed = 2023
-smote = SMOTE(random_state=seed, sampling_strategy='auto') #sampling_stragety는 smapling 비율 설정
+smote = SMOTE(random_state=seed, sampling_strategy='auto') #sampling_stragety는 sampling 비율 설정
 data_smote, target_smote = smote.fit_resample(data, target)
 
 df_data_up = pd.DataFrame(data_smote) #dataframe 구성
